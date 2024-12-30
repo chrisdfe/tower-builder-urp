28216c7a96103365f05cb31be361c4e78f1355ff
Christopher Ferris <christopher.d.ferris@gmail.com>
2024-12-30 13:06:12 -0800

    Updated unity/vscode extensions; basic camera movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    static float MOVEMENT_SPEED = 1.5f;
+    static float MOVEMENT_AMOUNT = 1.2f;
+
+    Vector3 targetPosition;
+    float movementTimer = 0;
+
+    void Awake()
+    {
+        targetPosition = transform.position;
+    }
+
+    void Update()
+    {
+        bool shouldResetTimer = false;
+
+        // Input
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            targetPosition += Vector3.up * MOVEMENT_AMOUNT;
+            shouldResetTimer = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            targetPosition += Vector3.left * MOVEMENT_AMOUNT;
+            shouldResetTimer = true;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            targetPosition += Vector3.down * MOVEMENT_AMOUNT;
+            shouldResetTimer = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            targetPosition += Vector3.right * MOVEMENT_AMOUNT;
+            shouldResetTimer = true;
+        }
+
+        // Movement
+        if (shouldResetTimer)
+        {
+            movementTimer = 0;
+        }
+        else
+        {
+            movementTimer += Time.deltaTime / MOVEMENT_SPEED;
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, movementTimer);
+    }
+}
